d2f5bedb69ff764b751fecb6daf9f24fa266fa3f
Laurence Lin <laurence.lin@icloud.com>
2019-06-12 08:00:00 -0400

    Update wrtds.R

--- wrtds.R
+++ wrtds.R
@@ -5,7 +5,7 @@ source('https://raw.githubusercontent.com/laurencelin/Date_analysis/master/LIB_m
 
 WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, Qhalfwin=2){ 
 	
-	return <- sapply(seq_len(dim(predData)[1]), function(i){
+	prediction <- sapply(seq_len(dim(predData)[1]), function(i){
 		if(is.na(predData$logQ[i]) ){
 			return <- rep(NA, replicationN)
 		}else{
@@ -36,7 +36,7 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
 			Tweight = Yweight * Sweight * Qweight * (!is.na(obsData$logC)) * (!is.na(obsData$logQ)); # sum(Tweight>0)
 			cond = Tweight>0; #sum(cond); sum(Yweight>0); sum(Sweight>0); sum(Qweight>0)
 			
-			return <- sapply(seq_len(replicationN), function(gg){
+			dailyReplication <- sapply(seq_len(replicationN), function(gg){
 					booststrap = sample(obsData$index[cond],sum(cond),replace=T)
 					result = lm(logC~ydecimal+logQ+sin2pit+cos2pit, data=obsData[booststrap,], weights= Tweight[booststrap])
 					# log(obsData$no3) = beta0 + beta1*obsData$ydecimal + beta2*obsData$logQ + beta3*obsData$sin2pit + beta4*obsData$cos2pit
@@ -47,11 +47,21 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
 					return <- c(
 						alpha * exp( result$coefficients[1] + sum(result$coefficients[2:5]*predData[i,c('ydecimal','logQ','sin2pit','cos2pit')]) ),
 						result$coefficients[3], # coefficent for the log(Q)
-						sum(Yweight>0 & Sweight>0 & Qweight>0),
 						summary(result)$r.squared)
-				})#sapply
+				})#sapply: every day and replication
+			# dailyReplication is a matrix: row is return and col is replication
+			#row[1] # WRTDS prediction
+			#row[2] # coefficent beta2
+			#row[3] # r2
+			
+			return <- c(dailyReplication[1,], dailyReplication[2,], dailyReplication[3,])
 		}#ifelse
 	})# sapply
+	# prediction is a matrix: col is daily; row is [1:replication] [1:replication] [1:replication]
+	return <- list(
+		conc = prediction[1:replication,],
+		beta2 = prediction[(1:replication)+replication,],
+		r2 = prediction[(1:replication)+2*replication,])
 }# function
 
  		
