c15cfd124af55e0fa62fd0ca3f5333be672c3204
Laurence Lin <laurence.lin@icloud.com>
2020-05-22 15:12:57 -0400

    added process bar

--- wrtds.R
+++ wrtds.R
@@ -13,6 +13,7 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
     # B. obsData should not have empty rows or missing-data rows.
     # C. users are responsible to pass quality data for the calculation below
     
+    print("forming data ...")
     obsData$index = seq_len(dim(obsData)[1]);
     obsData$ydecimal = dailyTimeSeries(obsData$date)$ydecimal
     obsData$sin2pit = sin(2*pi* obsData$ydecimal)
@@ -34,7 +35,9 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
     
     thresholdNUM = 100
     
+    
     ## scanning model results
+    print("begin to analysis ...")
     pb <- txtProgressBar(min = 0, max = dim(predData)[1], style = 3)
 	prediction <- lapply(seq_len(dim(predData)[1]), function(i){ #
         
@@ -67,7 +70,7 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
         Tweight = Yweight * Sweight * Qweight * (!is.na(obsData$logC)) * (!is.na(obsData$logQ)); # sum(Tweight>0)
         cond = Tweight>0; #sum(cond); sum(Yweight>0); sum(Sweight>0); sum(Qweight>0)
         
-        dailyReplication <- sapply(seq_len(replicationN), function(gg){
+        dailyReplication <- do.call(rbind,lapply(seq_len(replicationN), function(gg){
                 checkresults = NULL
                 while( is.null(checkresults) ){
                     tryCatch({
@@ -77,12 +80,13 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
                         #    booststrap = sample(obsData$index[cond],sum(cond),replace=T)
                         #}#while
                         booststrap = sample(obsData$index[cond],size = 80)
-                        result = lm(logC~ydecimal+logQ+sin2pit+cos2pit, data=obsData[booststrap,], weights= Tweight[booststrap])
-                        # log(obsData$no3) = beta0 + beta1*obsData$ydecimal + beta2*obsData$logQ + beta3*obsData$sin2pit + beta4*obsData$cos2pit
+                        result = lm(logConc~ydecimal+logQ+sin2pit+cos2pit, data=obsData[booststrap,], weights= Tweight[booststrap])
+                        # log(obsData$no3) = beta0 + beta1*obsData$ydecimal + beta2*obsData$logQ + (beta3*obsData$sin2pit + beta4*obsData$cos2pit)
                         # new features coming: visual beta2 A:: {y:Q X:time}; countour beta2 by colors
                         # B:: monthly boxplot of beta2; C::percentile Q boxplot of beta2
                     
                         alpha = sum(exp(result$residuals)*Tweight[booststrap])/sum(Tweight[booststrap])
+                        
                         predConc = alpha * exp( result$coefficients[1] + sum(result$coefficients[2:5]*predData[i,c('ydecimal','logQ','sin2pit','cos2pit')]) )# mg/L
                         predFlux = predConc * predData[i,'Q']*1000 # g/s -> mg/s
                         
@@ -90,23 +94,24 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
                         predObsFlux = predObsConc * predData[i,'ObsQ']*1000 # g/s -> mg/s
                         
                         if(is.infinite(predConc) | predConc > 100*max(obsData$C[booststrap]) |  predConc < 0.01 *min(obsData$C[booststrap]) ){
-                            print(paste(
-                                alpha ,min(result$residuals),max(result$residuals),
-                                result$coefficients[1],
-                                result$coefficients[2],
-                                result$coefficients[3],
-                                result$coefficients[4],
-                                result$coefficients[5]));
+                            # print(paste(
+                                # alpha ,min(result$residuals),max(result$residuals),
+                                # result$coefficients[1],
+                                # result$coefficients[2],
+                                # result$coefficients[3],
+                                # result$coefficients[4],
+                                # result$coefficients[5]));
                             checkresults <- NULL;
                         }else{
                             checkresults <- c(
-                                predConc,
-                                predFlux,
-                                result$coefficients[3], # coefficent for the log(Q)
-                                sum(result$coefficients[4:5]*predData[i,c('sin2pit','cos2pit')]),
-                                summary(result)$r.squared,
-                                predObsConc,
-                                predObsFlux);
+                                conc = as.numeric(predConc),
+                                flux = as.numeric(predFlux),
+                                ycoef = as.numeric(result$coefficients[2]), # coefficent for year
+                                qcoef = as.numeric(result$coefficients[3]), # coefficent for the log(Q)
+                                season = as.numeric(sum(result$coefficients[4:5]*predData[i,c('sin2pit','cos2pit')])),
+                                r2 = as.numeric(summary(result)$r.squared),
+                                predObsC = as.numeric(predObsConc),
+                                predObsF = as.numeric(predObsFlux));
                         }# end of if
                     },warning = function(w){
                         checkresults <- NULL
@@ -115,7 +120,7 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
                     })# tryCatch
                 }#while
                 return <- checkresults
-            })#sapply: every day and replication
+            }))#sapply: every day and replication
         # dailyReplication is a matrix: row is return and col is replication
         #row[1] # WRTDS conc prediction
         #row[2] # WRTDS flux predictions
@@ -123,20 +128,20 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
         #row[4] # seasonal signal
         #row[5] # r2
         
+        setTxtProgressBar(pb, i)
         return <- list(
-                    predictedConc = dailyReplication[1,], # conc
-                    predictedFlux = dailyReplication[2,],
-                    Qcoef = dailyReplication[3,], # beta2
-                    SeasonalSignal = dailyReplication[4,], # seasonal
-                    CQr2 = dailyReplication[5,], # r2
-                    predictedObsConc = dailyReplication[6,],
-                    predictedObsFlux = dailyReplication[7,],
+                    predictedConc = dailyReplication[,'conc'], # conc
+                    predictedFlux = dailyReplication[,'flux'],
+                    Ycoef = dailyReplication[,'ycoef'], # beta1
+                    Qcoef = dailyReplication[,'qcoef'], # beta2
+                    SeasonalSignal = dailyReplication[,'season'], # seasonal
+                    CQr2 = dailyReplication[,'r2'], # r2
+                    predictedObsConc = dailyReplication[,'predObsC'],
+                    predictedObsFlux = dailyReplication[,'predObsF'],
                     actualYhalfwin = Yhalfwin_,
                     actualShalfwin = Shalfwin_,
                     actualQhalfwin = Qhalfwin_)
-    
-        setTxtProgressBar(pb, i)
-	})# lapply - daily loop
+	})# lapply - daily loop -- prediction
 	# prediction is a matrix: col is daily; row is [1:replication] [1:replication] [1:replication] 1 1 1
 	# prediction is a list of daily "return list"
     
@@ -150,18 +155,34 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
 	concNSE = sapply(1:replicationN,function(ii){ NSE(obsData$C, extracted_predictedObsConc[,ii]) })
     fluxNSE = sapply(1:replicationN,function(ii){ NSE(obsData$C*obsData$Q*1000, extracted_predictedObsFlux[,ii]) })
 	
-    return <- list(
-		PredictedConc = prediction[1:replicationN,],
-        PredictedFlux = prediction[1:replicationN,],
-		Qcoef = prediction[(1:replicationN)+replicationN,],
-		SeasonalSignal = prediction[(1:replicationN)+2*replicationN,],
-		CQ_r2 = prediction[(1:replicationN)+3*replicationN,],
-        Yhalfwin = mean(prediction[4*replicationN+1,]),
-        Shalfwin = mean(prediction[4*replicationN+2,]),
-        Qhalfwin = mean(prediction[4*replicationN+3,]),
-        # ...
-        ConcNSE4obs = concNSE, # concentration
-        fluxNSE4obs = fluxNSE )
+	# prediction is a list of daily list
+	# report mean daily conc and upper and lower bound
+	return <- list(
+		ConcPrediction = do.call(rbind,lapply(seq_along(prediction),function(ii){
+			return <- c(
+				mConc = mean(prediction[[ii]]$predictedConc), 
+				lConc = quantile(prediction[[ii]]$predictedConc,0.025),
+				uConc = quantile(prediction[[ii]]$predictedConc,1-0.025) )})),
+				
+		FluxPrediction = do.call(rbind,lapply(seq_along(prediction),function(ii){
+			return <- c(
+				mFlux = mean(prediction[[ii]]$predictedFlux),
+				lFlux = quantile(prediction[[ii]]$predictedFlux,0.025),
+				uFLux = quantile(prediction[[ii]]$predictedFlux,1-0.025) )})),
+		
+		modelParam = do.call(rbind,lapply(seq_along(prediction),function(ii){
+			return <- c(
+				Ycoef = mean(prediction[[ii]]$Ycoef),
+				Qcoef = mean(prediction[[ii]]$Qcoef),
+				Season = mean(prediction[[ii]]$SeasonalSignal),
+				Yhalfwin = mean(prediction[[ii]]$actualYhalfwin), 
+				Shalfwin = mean(prediction[[ii]]$actualShalfwin), 
+				Qhalfwin = mean(prediction[[ii]]$actualQhalfwin),
+				dailyCQr2 = mean(prediction[[ii]]$CQr2) )})),
+		
+		modelFittiness = c(concNSE, fluxNSE)			
+	)# end of return list 
+	
 }# function
 
  		
