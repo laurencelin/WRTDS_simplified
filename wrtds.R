79a8889972d8984bca757405506f69976d952d91
Laurence Lin <laurence.lin@icloud.com>
2020-05-22 13:01:17 -0400

    simplify replication procedure

--- wrtds.R
+++ wrtds.R
@@ -32,6 +32,8 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
     predData$ObsQ[pred2ObsCOND] = obsData$Q
     predData$logObsQ[pred2ObsCOND] = obsData$logQ
     
+    thresholdNUM = 120
+    
 	prediction <- lapply(seq_len(dim(predData)[1]), function(i){ #
         
         # i is every day
@@ -39,7 +41,7 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
         Yhalfwin_ = Yhalfwin
         Ydist = abs(obsData$ydecimal - predData$ydecimal[i])
         Yweight = (Ydist<=Yhalfwin)*(1-(Ydist/Yhalfwin)^3)^3
-            while(sum(Yweight>0)<100){
+            while(sum(Yweight>0)<thresholdNUM){
                 Yhalfwin_ = Yhalfwin_*1.1
                 Yweight = (Ydist<= Yhalfwin_)*(1-(Ydist/Yhalfwin_)^3)^3
             }#while
@@ -47,7 +49,7 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
         Shalfwin_ = Shalfwin
         Sdist = vectorMin( ceiling(Ydist)-Ydist, Ydist-floor(Ydist))
         Sweight = (Sdist <= Shalfwin)*(1-(Sdist/Shalfwin)^3)^3
-            while(sum(Yweight>0 & Sweight>0)<100){
+            while(sum(Yweight>0 & Sweight>0)<thresholdNUM){
                 Shalfwin_ = Shalfwin_*1.1
                 Sweight = (Sdist <= Shalfwin_)*(1-(Sdist/Shalfwin_)^3)^3
             }#while
@@ -55,7 +57,7 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
         Qhalfwin_ = Qhalfwin
         Qdist = abs(obsData$logQ - predData$logQ[i])
         Qweight = (Qdist <= Qhalfwin_)*(1-(Qdist/Qhalfwin_)^3)^3
-            while(sum(Yweight>0 & Sweight>0 & Qweight>0)<100){
+            while(sum(Yweight>0 & Sweight>0 & Qweight>0)<thresholdNUM){
                 Qhalfwin_ = Qhalfwin_*1.1
                 Qweight = (Qdist <= Qhalfwin_)*(1-(Qdist/Qhalfwin_)^3)^3
             }# while
@@ -67,11 +69,12 @@ WRTDS = function(obsData, predData, replicationN=50, Yhalfwin=10, Shalfwin=0.5, 
                 checkresults = NULL
                 while( is.null(checkresults) ){
                     tryCatch({
-                        booststrap = rep(1,sum(cond))
-                        while( max(table(booststrap))>4 ){
-                            # no data can be repeated more than three times
-                            booststrap = sample(obsData$index[cond],sum(cond),replace=T)
-                        }#while
+                        #booststrap = rep(1,sum(cond))
+                        #while( max(table(booststrap))>4 ){
+                        #    # no data can be repeated more than three times
+                        #    booststrap = sample(obsData$index[cond],sum(cond),replace=T)
+                        #}#while
+                        booststrap = sample(obsData$index[cond],100)
                         result = lm(logC~ydecimal+logQ+sin2pit+cos2pit, data=obsData[booststrap,], weights= Tweight[booststrap])
                         # log(obsData$no3) = beta0 + beta1*obsData$ydecimal + beta2*obsData$logQ + beta3*obsData$sin2pit + beta4*obsData$cos2pit
                         # new features coming: visual beta2 A:: {y:Q X:time}; countour beta2 by colors
